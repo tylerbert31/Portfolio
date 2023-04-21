174e4e231d0b003bbd39b6e7f582d6ee50232e4c
Tyler Bert Baring <tbaring_ccs@uspf.edu.ph>
2023-04-21 12:41:36 +0800

    Added Loader and Mobile view

--- baring-portfolio/src/App.js
+++ baring-portfolio/src/App.js
@@ -2,10 +2,27 @@ import "./App.css"
 import About from "./components/about/about"
 import Intro from "./components/intro/intro"
 import { Navbar } from "./components/navbar/navbar.js"
+import { useState, useEffect } from "react"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+  })
+
   return (
     <>
+      {loading ? (
+        <div className="loader">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        ""
+      )}
+
       <div id="navbar">
         <Navbar />
       </div>
